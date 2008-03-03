d64fa8fef67f3756dfc65f47a29a12e10ba9b440
colin chun <caleon@gmail.com>
2008-03-02 20:39:19 -0800

    fixing errors.rb following failed unit test

--- config/initializers/errors.rb
+++ config/initializers/errors.rb
@@ -1,15 +1,25 @@
 class LimitError < StandardError; end
 class FriendshipError < StandardError
-  MESSAGES = { :forbidden   =>  [nil, "Friendship requests are forbidden"],
-               :pending     =>  [:friend_ids, "is invalid: %s already has a friendship request with %s", :user, :friend],
-               :already     =>  [:friend_ids, "is invalid: %s is already friends with %s", :user, :friend]}
+  class MsgSet
+    def initialize(field, msg, *vars)
+      @arr = [field, msg, *vars]
+    end
+
+    def field; @arr[0]; end
+    def msg_str; @arr[1]; end
+    def users; @arr[2..-1]; end
+  end
+  
+  MESSAGES = { :forbidden   =>  MsgSet.new(nil, "Friendship requests are forbidden"),
+               :pending     =>  MsgSet.new(:friend_ids, "is invalid: %s already has a friendship request with %s", :user, :friend),
+               :already     =>  MsgSet.new(:friend_ids, "is invalid: %s is already friends with %s", :user, :friend) }
                
   ERROR_TARGETS = { :forbidden    =>  :user,
                     :pending      =>  :user,
                     :already      =>  :user }
   
   attr_accessor :kind, :user, :friend
-
+  
   def initialize(*args)
     @kind = args.shift if args.first.is_a?(Symbol)
     @user = args.first
@@ -18,16 +28,17 @@ class FriendshipError < StandardError
   
   def add_error
     msg_set = MESSAGES[@kind]
-    field = msg_set.shift
-    msg_str = msg_set.shift
-    msg_user_names = msg_set.map {|sym| instance_variable_get(:"@#{sym}").internal_name }
-    msg = msg_str % msg_user_names
-    error_args = field ? [:add, field, msg] : [:add_to_base, msg]
+    msg = msg_set.msg_str % msg_set.users.map {|sym| get_user(sym) }
+    error_args = msg_set.field ? [:add, msg_set.field, msg] : [:add_to_base, msg]
     erroring_user.errors.send(*error_args)
   end
   
+  def get_user(sym)
+    instance_variable_get(:"@#{sym}")
+  end
+  
   def erroring_user
-    instance_variable_get(:"@#{ERROR_TARGETS[@kind]}")
+    get_user(ERROR_TARGETS[@kind])
   end
   
 end
