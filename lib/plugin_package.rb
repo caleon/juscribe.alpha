66701d68a6ed09909bcd5597ab093e70812b9984
colin chun <caleon@gmail.com>
2008-03-03 16:15:29 -0800

    removing unnecessary call

--- lib/plugin_package.rb
+++ lib/plugin_package.rb
@@ -1,7 +1,7 @@
 module PluginPackage
   def self.included(base)
     ['acts_as_accessible', 'acts_as_responsible', 'acts_as_taggable',  'acts_as_widgetable'].each do |plugin|
-      base.send(plugin.intern)
+      base.send(plugin)
     end
     base.send(:include, Layoutable)
   end
