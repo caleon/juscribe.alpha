04e3ef171b981b97382ebd2aef714aeb047c87b4
colin chun <caleon@gmail.com>
2008-03-08 00:19:17 -0800

    attachment_fu db file in rails_root ignored

--- config/initializers/logger.rb
+++ config/initializers/logger.rb
@@ -1,9 +1,12 @@
-ActionController::Base.logger = Logger.new("#{RAILS_ROOT}/log/#{RAILS_ENV}_controller.log")
+ActionController::Base.logger =
+          Logger.new("#{RAILS_ROOT}/log/#{RAILS_ENV}_controller.log")
 
-ActiveRecord::Base.logger = Logger.new("#{RAILS_ROOT}/log/#{RAILS_ENV}_database.log")
+ActiveRecord::Base.logger =
+          Logger.new("#{RAILS_ROOT}/log/#{RAILS_ENV}_database.log")
 # ActiveRecord::Base.logger = Logger.new("#{RAILS_ROOT}/log/#{RAILS_ENV}_database.log", 'daily')
 
-ActionMailer::Base.logger = Logger.new("#{RAILS_ROOT}/log/#{RAILS_ENV}_mailer.log")
+ActionMailer::Base.logger =
+          Logger.new("#{RAILS_ROOT}/log/#{RAILS_ENV}_mailer.log")
 
 class Logger
   def format_message(severity, timestamp, progname, msg)
