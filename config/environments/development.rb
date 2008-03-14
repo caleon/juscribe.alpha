ff4cef2b9204a424a4d59a268bb0e21ad04cf3e8
colin chun <caleon@gmail.com>
2008-03-14 08:50:15 -0700

    minor fixes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@
 # In the development environment your application's code is reloaded on
 # every request.  This slows down response time but is perfect for development
 # since you don't have to restart the webserver when you make code changes.
-config.cache_classes = false
+config.cache_classes = true
 
 # Log error messages when you accidentally call methods on nil.
 config.whiny_nils = true
@@ -17,4 +17,4 @@ config.action_view.cache_template_extensions         = false
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 
-Dependencies.log_activity = true
+#Dependencies.log_activity = true
