57041bbd62a148279f74b98272b593f243709371
colin chun <caleon@gmail.com>
2008-03-14 03:37:09 -0700

    Revert "dealing with the controller load issue. apparently removing the custom load_paths configuration will fix my controller issue."
    This reverts commit 7a683c1788accd6678459cddd96016e2932c5f45.

--- config/environment.rb
+++ config/environment.rb
@@ -27,7 +27,7 @@ Rails::Initializer.run do |config|
 
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
-  #config.load_paths += %W( #{RAILS_ROOT}/app/models/list #{RAILS_ROOT}/app/models/item #{RAILS_ROOT}/app/models/response )
+  config.load_paths += %W( #{RAILS_ROOT}/app/models/list #{RAILS_ROOT}/app/models/item #{RAILS_ROOT}/app/models/response )
 
   # Force all environments to use the same logger level
   # (by default production uses :info, the others :debug)
