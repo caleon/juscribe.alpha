aa2c9a019eb79079ea06310f9871ffd784d6b414
colin chun <caleon@gmail.com>
2008-03-19 07:37:41 -0700

    got back my domain name! more layout work

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -3,3 +3,4 @@ custom_date_formats = {
   :medium => "%b %e, %Y" 
 } 
 ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(custom_date_formats)
+ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(custom_date_formats)
