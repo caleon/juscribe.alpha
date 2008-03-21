bada389743c34ae6bbbf4c90c7f4b5a5d5017f8f
colin chun <caleon@gmail.com>
2008-03-20 18:28:27 -0700

    rounded corners ftw

--- app/helpers/css_helper.rb
+++ app/helpers/css_helper.rb
@@ -12,4 +12,10 @@ module CssHelper
   def br_clear
     '<br style="clear: both;" />'
   end
+  
+  def rounded_shim(position=nil)
+    %{<div class="msm_shim msm_shim#{position ? "_#{position}" : nil}">
+  			<div>&nbsp;</div>
+  		</div>}
+  end
 end
