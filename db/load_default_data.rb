aa8af769611dbb42ecc21440bf2b6f638e3c9921
colin chun <caleon@gmail.com>
2008-03-27 17:53:00 -0700

    default data script doesn't work right now. message of the day byline was dependent on articles table having an entry setting up after deciding depictable scheme

--- db/load_default_data.rb
+++ db/load_default_data.rb
@@ -4,12 +4,13 @@ class LoadDefaultData < ActiveRecord::Migration
   def self.up
     down
     dir = File.join(File.dirname(__FILE__), 'fixtures')
-    %( users ).each do |table|
+    %( users groups ).each do |table|
       Fixtures.create_fixtures(dir, table)
     end
   end
   
   def self.down
-    # User.delete_all
+    User.delete_all
+    Group.delete_all
   end
 end
--- lib/active_record/acts/depictable.rb
+++ lib/active_record/acts/depictable.rb
@@ -7,7 +7,7 @@ module ActiveRecord::Acts::Depictable
     def acts_as_depictable(options = {})
       with_options :class_name => 'Picture', :as => :depictable, :order => :position do |pic|
         pic.has_many :pictures, :dependent => :nullify, :conditions => 'pictures.thumbnail IS NULL'
-        pic.has_one :front_picture, :conditions => 'pictures.thumbnail IS NULL'
+        pic.has_one :primary_picture, :conditions => 'pictures.thumbnail IS NULL'
         pic.has_many :thumbs, :conditions => ["pictures.thumbnail = ?", 'thumb']
       end   
       
@@ -16,12 +16,7 @@ module ActiveRecord::Acts::Depictable
   end
   
   module InstanceMethods
-    def primary_picture
-      front_picture.thumbnails.find_by_thumbnail('feature')
-    end # i'm not liking the fact that three DB rows are created per pic. Plus updating attributes
-    # on one doesn't guarantee others are affected. Need to rethink this.
-    
-    # TODO: Finish rethinking and redo this.
+
   end
 end
 
