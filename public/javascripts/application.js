a81f6da1c549df86d2d1f4cfba8570e70a5f5902
colin chun <caleon@gmail.com>
2008-04-17 17:42:10 -0700

    browser storing what row was clicked (maintaining highlighted state)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,21 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+var Browser = Class.create()
+Browser.prototype = {
+	dom_el: '',
+	preview_pane: null,
+	lastClicked: {},
+	
+	initialize: function(dom_el, preview_pane){
+		this.dom_el = dom_el;
+		this.preview_pane = preview_pane;
+	},
+	setActiveLink: function(el, lvl){
+		Element.addClassName(el, 'active')
+		if (this.lastClicked[lvl] && this.lastClicked[lvl] != el) {
+			Element.removeClassName(this.lastClicked[lvl], 'active');
+		}
+		this.lastClicked[lvl] = el;
+		$(this.preview_pane + '-inner').innerHTML = 'Nothing selected';
+	}
+}
