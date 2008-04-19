1a186e7cc983fb5eb7fbcde830516c54257b5486
colin chun <caleon@gmail.com>
2008-04-19 03:17:13 -0700

    matching widget heights if there are two widgets on same row with varying height. this is done on MSM skin with javascript, but the same could not be accomplished on users-show because the ajax loading of the rss feed messes with calculation and prohibits columns (main and side) from getting equalized heights. although some more polishing is needed down the line, for the purposes of this ticket, the matching height attempt was a success on users-show and blogs-show. [#51 state:resolved]

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-var Browser = Class.create()
+var Browser = Class.create();
 Browser.prototype = {
 	dom_el: '',
 	preview_pane: null,
@@ -18,4 +18,39 @@ Browser.prototype = {
 		this.lastClicked[lvl] = el;
 		$(this.preview_pane + '-inner').innerHTML = 'Nothing selected';
 	}
-}
+};
+
+var MsmLayout = Class.create();
+MsmLayout.prototype = {
+	heightOffsets: ['padding-top', 'padding-bottom', 'border-top-width', 'border-bottom-width'],
+	initialize: function(){},
+	equalizeWidgetHeights: function(el1_id, el2_id){
+		els = [ $(el1_id), $(el2_id) ];
+		targetHeight = els.max(function(el){ return el.getHeight(); });
+		shortEl = els.find(function(el){ return el.getHeight() != targetHeight; });
+		diff = targetHeight - shortEl.getHeight();
+		
+		adjuster = shortEl.childElements().find(function(e){ return e.className.match(/inner/); });
+		otherHeights = this.getOtherHeights(adjuster);
+		trueHeight = adjuster.getHeight() - otherHeights;		
+		adjuster.style.height = trueHeight + diff + 'px';
+	},
+	
+	equalizeColumnHeights: function(col1_id, col2_id){
+		els = [ $(col1_id), $(col2_id) ];
+		targetHeight = els.max(function(el){ return el.getHeight(); });
+		shortEl = els.find(function(el){ return el.getHeight() != targetHeight; });
+		diff = targetHeight - shortEl.getHeight();
+		
+		adjuster = shortEl.childElements().last().childElements().find(function(el){ return el.className.match(/inner/); });
+		otherHeights = this.getOtherHeights(adjuster);
+		trueHeight = adjuster.getHeight() - otherHeights;
+		adjuster.style.height = trueHeight + diff + 'px';
+	},
+	
+	getOtherHeights: function(el){
+		return this.heightOffsets.inject(0, function(acc, s){
+			return acc + parseInt(el.getStyle(s));
+		});
+	}
+};
