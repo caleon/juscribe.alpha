6ff638d4ea58c26e8ddab6a1be2d351165a53c78
colin chun <caleon@gmail.com>
2008-04-21 00:09:10 -0700

    rss feed formatting with Hpricot, with its links and especially images. only showing first image it finds and adds it to before the markup of the feed item_content [#170 state:resolved]

--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -20,6 +20,7 @@ module FeedsHelper
                 :item_author, :item_content, :item_published_date, :item_guid
   end
   
+  # Why is this camelcased?
   def parseFeed(feed_url)
     rss_item = nil
     open(feed_url) do |http|
@@ -27,8 +28,8 @@ module FeedsHelper
       result = RSS::Parser.parse(response, false)
       latest = result.items.first
       rss_item = RssItem.new( { :link => result.channel.link, :title => result.channel.title },
-                                  { :link => latest.link, :title => latest.title, :author => latest.author,
-                                    :content => latest.description, :published_date => latest.pubDate, :guid => latest.guid } )
+                              { :link => latest.link, :title => latest.title, :author => latest.author,
+                                :content => latest.description, :published_date => latest.pubDate, :guid => latest.guid } )
     end
     return rss_item
   end
@@ -36,5 +37,20 @@ module FeedsHelper
   def rss_link_for(kind, path, opts={})
     str = "RSS Feed for #{kind}"
     link_to image_tag('shim.gif', :class => 'rss_image', :width => 27, :height => 15, :alt => 'RSS Icon'), path, :class => 'rss_link', :title => str
+  end
+  
+  def primary_picture_node(feed)
+    doc = Hpricot(feed.item_content)
+    (doc/"img").first
+  end
+    
+  def format_feed(feed)
+    doc = Hpricot(truncate_html(simple_format(sanitize(feed.item_content, :tags => %w( a img em i strong b )), :class => 'articleContent'), 500))
+    first_p = (doc/".articleContent").first
+    (doc/"img").remove
+    first_p.inner_html = primary_picture_node(feed).to_html + first_p.inner_html
+    (doc/"a:empty").remove # In case the removed images were wrapped in a link
+    (doc/"a").each {|link| link.set_attribute('class', 'external') and link.set_attribute('target', '_new') if link.attributes['href'].match(/:\/\//) }
+    doc.to_html
   end
 end
