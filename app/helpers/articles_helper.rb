55a72984b9458b1c59a90b001a145c79607a5d2a
colin chun <caleon@gmail.com>
2008-04-26 05:44:17 -0700

    cleaning up format_article and its helpers [#17]

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -19,10 +19,22 @@ module ArticlesHelper
     instance_eval %{ #{opts[:prefix] ? "#{opts[:prefix]}_" : ''}#{object.path_name_prefix}_latest_articles_url(object.to_path(true)) }
   end
   
+  # FIXME: If the article record no longer exists, this will error out.
+  def articles_history(limit=5)
+    Article.find(session[:articles_history] ||= []).compact.sort_by {|art| session[:articles_history].index(art.id) }
+  end
+  
+  
+  ################################
+  ##     ARTICLE FORMATTING     ##
+  ################################
+  
+  # TODO: A blog can remove/customize this intro on premium accounts
   def article_intro
     content_tag :strong, "(#{APP[:name].upcase}) &mdash;", :class => 'articleIntro'
   end
   
+  # Here is where to add more types of components per article
   def article_components_for(article)
     article.pictures[1..-1] || []
   end
@@ -31,7 +43,6 @@ module ArticlesHelper
     case text_or_model
     when Picture
       if feature = text_or_model.thumbnails.find_by_thumbnail('feature')
-#        image_tag(thumb.public_filename, :class => dom_class(thumb) + ' articleComponent ' + cycle('right', 'left'))
         zoomable_picture_with(feature, :article_id => article.id, :with => { :class => "articleComponent #{cycle('right', 'left')}" })
       end
     end
@@ -62,50 +73,29 @@ module ArticlesHelper
   #
   # Lastly, an empty SPAN element is added to the end of the .articleContent element to be populated
   # via Javascript with comments corresponding to that paragraph.
+  #
+  # NOTE: The following helper methods need to be in a certain order to avoid conflicting with each other.
+  # TODO: Should also Hpricot the input before we save to database on Article#save.
   def format_article(article, opts={})
+    opts[:article] ||= article || @article
     text = article.content
-    text = article_intro + article.content unless opts[:without_intro]
-    # Create desired DOM hierarchy and convert to Hpricot
+    text = article_intro + text unless opts[:without_intro]
     formatted = Hpricot(p_wrap(text))
-    # Escape html tags within PRE or CODE
-    (formatted/"pre, code").each do |code_el|
-      code_el.inner_html = code_el.inner_html.gsub(/\n*<br \/>\n*/, "\n")
-      code_el.inner_html = html_escape(code_el.inner_html)
-    end
-    # Make external links styled differently and open in new window
-    (formatted/"a").each {|link| link.set_attribute('class', 'external') and link.set_attribute('target', '_new') if link.attributes['href'].match(/:\/\/\w+\.[^(juscribe\.com)](?:\/.*)?$/)} # Test forging with juscribe.com.hacker.com
+    escape_code_html(formatted)
+    clarify_external_links(formatted)
     # Remove unwanted html attributes like onclick
     text = sanitize(formatted.to_html, :tags => allowed_tags(opts[:truncate]), :attributes => allowed_attributes)
     hpricot = Hpricot(text)
-    # Set class and unique IDs for each block level element for this articleContent
-    @component_count = 0
-    @aggregate_length = 0
-    hpricot.each_child_with_index do |child, i|
-      if child.elem?
-        child.set_attribute('class', 'articleContent')
-        paragraph_id = "#{opts[:prefix] ? "#{opts[:prefix]}_" : ''}article-#{article.id}-paragraph-#{Digest::SHA1.hexdigest(child.inner_html)[0..6]}"
-        child.set_attribute('id', paragraph_id)
-        child.set_attribute('style', "z-index: #{50 - i};")
-        if @aggregate_length > 1400 && comp = article_components_for(article)[@component_count] 
-          child.inner_html = render_article_component(article, comp) + child.inner_html
-          @aggregate_length = child.inner_text.chars.length
-          @component_count += 1
-        else
-          @aggregate_length += child.inner_text.chars.length
-        end
-        child.inner_html += content_tag(:span, "&nbsp;", :class => paragraph_id + "-comment mixedComment") unless opts[:truncate] || child.name == 'pre'
-      end
-    end
-    text = opts[:truncate] ? truncate_html(hpricot.to_s, opts[:truncate]) : hpricot.to_s
-    # Return finished
-    text.to_s
+    prep_with_mixins(hpricot, opts)
+    opts[:truncate] ? truncate_html(hpricot.to_s, opts[:truncate]) : hpricot.to_s
   end
   
-  # TODO: Need to Hpricot the input before we save to database on Article#save
+  # Create desired DOM hierarchy
   def p_wrap(text, opts={})
     # Actually if opts[:truncate], we might wanna remove the bad tags entirely instead of escaping...
     my_allowed_tags = opts[:tags].is_a?(Array) ? opts[:tags] : (allowed_tags - %w(p))
     # Basically these are types that cannot exist within a P tag. P is not allowed, however.
+    # TODO: Style h1-h3 so that they are at greatest the size of an h3.
     block_levels = "pre|blockquote|h1|h2|h3|h4|h5|h6|ol|ul"
     res = text.to_s.
           gsub(/(<\/?(\w+)[^>]*>)/) {|t| my_allowed_tags.include?($2) ? $1 : h($1)}.
@@ -117,10 +107,55 @@ module ArticlesHelper
         gsub(/\s*<p><\/p>\s*/, "\n").
         gsub(/([^\n|>]\n)(?!\n)/, "\\1<br />\n").strip
   end
-
   
-  # FIXME: if the article record no longer exists, this will error out
-  def articles_history(limit=5)
-    Article.find(session[:articles_history] ||= []).compact.sort_by {|art| session[:articles_history].index(art.id) }
+  # Escape html tags within PRE or CODE
+  def escape_code_html(hpricot)
+    (hpricot/"pre, code").each do |code_el|
+      # FIXME: It's possible the following won't remove the <p> tags generated by p_wrap...
+      code_el.inner_html = code_el.inner_html.gsub(/\n*<br \/>\n*/, "\n")
+      code_el.inner_html = html_escape(code_el.inner_html)
+    end
+  end
+  
+  # Make external links styled differently and open in new window
+  def clarify_external_links(hpricot)
+    (hpricot/"a").each {|link| link.set_attribute('class', 'external') and link.set_attribute('target', '_new') if link.attributes['href'].match(/:\/\/\w+\.[^(juscribe\.com)].*$/)} # Test forging with juscribe.com.hacker.com
+  end
+  
+  # Set class and unique IDs for each block level element for this articleContent
+  def prep_with_mixins(hpricot, opts={})
+    raise ArgumentError unless article = opts[:article] || @article
+    component_count, aggregate_length = 0, 0
+    hpricot.each_child_with_index do |child, i|
+      if child.elem?
+        # IMPORTANT: the following digest code cannot change, else it will mess up associations
+        # It should also be set before modifications to the entire element, like class, id, inner_html...
+        paragraph_id = paragraph_id_for(child, opts) unless opts[:truncate]
+        child.set_attribute('class', 'articleContent')
+        unless opts[:truncate]
+          child.set_attribute('id', paragraph_id)
+          child.set_attribute('style', "z-index: #{50 - i/2};")
+          if aggregate_length > 1400 && comp = article_components_for(article)[component_count] 
+            child.inner_html = render_article_component(article, comp) + child.inner_html
+            aggregate_length = child.inner_text.chars.length
+            component_count += 1
+          else
+            aggregate_length += child.inner_text.chars.length
+          end
+          create_comment_mixin(child, paragraph_id) unless child.name == 'pre'
+        end
+      end
+    end
+  end
+  
+  def paragraph_id_for(hp_el, opts={})
+    raise ArgumentError unless article = opts[:article] || @article
+    "#{opts[:prefix] ? "#{opts[:prefix]}_" : ''}" +
+    "article-#{article.id}-" + 
+    "paragraph-#{Digest::SHA1.hexdigest(hp_el.to_s)[0..6]}"
+  end
+
+  def create_comment_mixin(hp_el, paragraph_id)
+    hp_el.inner_html += content_tag(:span, "&nbsp;", :class => paragraph_id + "-comment article-mixedComment mixedComment")
   end
 end
