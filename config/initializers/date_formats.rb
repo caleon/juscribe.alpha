8a6aebc2e763b7586058d25fda75597d94156879
colin chun <caleon@gmail.com>
2009-03-26 14:49:12 -0700

    adding timestamps to _message partial for listing and showing individual. also working on time zone stuff. [#210 state:resolved tagged:committed]

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,5 +1,7 @@
 custom_date_formats = { 
   :concise => "%d.%b.%y", 
+  :this_year => "%b %e at %l:%M%p",
+  :not_this_year => "%b %e, %Y at %l:%M%p",
   :medium => "%b %e, %Y" 
 } 
 ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(custom_date_formats)
